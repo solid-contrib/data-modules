9fb01b74ac2e414c8d67ff4fce673ecb597aef48
Angelo Veltens <angelo.veltens@online.de>
2023-11-08 21:25:24 +0100

    test: query name-email index

--- contacts/src/rdflib/AddressBookQuery.spec.ts
+++ contacts/src/rdflib/AddressBookQuery.spec.ts
@@ -1,6 +1,6 @@
 import { AddressBookQuery } from "./AddressBookQuery";
 import { blankNode, graph, sym } from "rdflib";
-import { dc } from "./namespaces";
+import { dc, vcard } from "./namespaces";
 
 describe("AddressBookQuery", () => {
   describe("query title", () => {
@@ -56,6 +56,68 @@ describe("AddressBookQuery", () => {
       const query = new AddressBookQuery(store, addressBookNode);
       const title = query.queryTitle();
       expect(title).toBe("");
+    });
+  });
+
+  describe("query name-email index", () => {
+    it("returns null if nothing found in store", () => {
+      const query = new AddressBookQuery(
+        graph(),
+        sym("http://pod.test/alice/contacts/index.ttl#this"),
+      );
+      const result = query.queryNameEmailIndex();
+      expect(result).toBe(null);
+    });
+
+    it("returns the node found in store", () => {
+      const store = graph();
+      let addressBookNode = sym(
+        "http://pod.test/alice/contacts/index.ttl#this",
+      );
+      const peopleNode = sym("http://pod.test/alice/contacts/people.ttl");
+      store.add(
+        addressBookNode,
+        vcard("nameEmailIndex"),
+        peopleNode,
+        addressBookNode.doc(),
+      );
+      const query = new AddressBookQuery(store, addressBookNode);
+      const result = query.queryNameEmailIndex();
+      expect(result).toBe(peopleNode);
+    });
+
+    it("returns null if statement is from wrong document", () => {
+      const store = graph();
+      let addressBookNode = sym(
+        "http://pod.test/alice/contacts/index.ttl#this",
+      );
+      const peopleNode = sym("http://pod.test/alice/contacts/people.ttl");
+      store.add(
+        addressBookNode,
+        vcard("nameEmailIndex"),
+        peopleNode,
+        sym("http://pod.test/wrong/doc"),
+      );
+      const query = new AddressBookQuery(store, addressBookNode);
+      const title = query.queryNameEmailIndex();
+      expect(title).toBe(null);
+    });
+
+    it("returns null if statement is from wrong address book", () => {
+      const store = graph();
+      let addressBookNode = sym(
+        "http://pod.test/alice/contacts/index.ttl#this",
+      );
+      const peopleNode = sym("http://pod.test/alice/contacts/people.ttl");
+      store.add(
+        sym("http://pod.test/alice/wrong/index.ttl#this"),
+        vcard("nameEmailIndex"),
+        peopleNode,
+        addressBookNode.doc(),
+      );
+      const query = new AddressBookQuery(store, addressBookNode);
+      const result = query.queryNameEmailIndex();
+      expect(result).toBe(null);
     });
   });
 });
