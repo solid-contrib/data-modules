5cd68b1936bc7a9588ef2c28e0c28e15a6da83be
Angelo Veltens <angelo.veltens@online.de>
2023-11-08 21:39:58 +0100

    test: query contacts

--- contacts/src/rdflib/AddressBookQuery.spec.ts
+++ contacts/src/rdflib/AddressBookQuery.spec.ts
@@ -1,5 +1,5 @@
 import { AddressBookQuery } from "./AddressBookQuery";
-import { blankNode, graph, sym } from "rdflib";
+import { graph, sym } from "rdflib";
 import { dc, vcard } from "./namespaces";
 
 describe("AddressBookQuery", () => {
@@ -14,7 +14,7 @@ describe("AddressBookQuery", () => {
     });
     it("returns the dc:title found in store", () => {
       const store = graph();
-      let addressBookNode = sym(
+      const addressBookNode = sym(
         "http://pod.test/alice/contacts/index.ttl#this",
       );
       store.add(
@@ -29,7 +29,7 @@ describe("AddressBookQuery", () => {
     });
     it("returns an empty string if title is from wrong document", () => {
       const store = graph();
-      let addressBookNode = sym(
+      const addressBookNode = sym(
         "http://pod.test/alice/contacts/index.ttl#this",
       );
       store.add(
@@ -44,7 +44,7 @@ describe("AddressBookQuery", () => {
     });
     it("returns an empty string if title is from wrong address book", () => {
       const store = graph();
-      let addressBookNode = sym(
+      const addressBookNode = sym(
         "http://pod.test/alice/contacts/index.ttl#this",
       );
       store.add(
@@ -71,7 +71,7 @@ describe("AddressBookQuery", () => {
 
     it("returns the node found in store", () => {
       const store = graph();
-      let addressBookNode = sym(
+      const addressBookNode = sym(
         "http://pod.test/alice/contacts/index.ttl#this",
       );
       const peopleNode = sym("http://pod.test/alice/contacts/people.ttl");
@@ -88,7 +88,7 @@ describe("AddressBookQuery", () => {
 
     it("returns null if statement is from wrong document", () => {
       const store = graph();
-      let addressBookNode = sym(
+      const addressBookNode = sym(
         "http://pod.test/alice/contacts/index.ttl#this",
       );
       const peopleNode = sym("http://pod.test/alice/contacts/people.ttl");
@@ -105,7 +105,7 @@ describe("AddressBookQuery", () => {
 
     it("returns null if statement is from wrong address book", () => {
       const store = graph();
-      let addressBookNode = sym(
+      const addressBookNode = sym(
         "http://pod.test/alice/contacts/index.ttl#this",
       );
       const peopleNode = sym("http://pod.test/alice/contacts/people.ttl");
@@ -118,6 +118,218 @@ describe("AddressBookQuery", () => {
       const query = new AddressBookQuery(store, addressBookNode);
       const result = query.queryNameEmailIndex();
       expect(result).toBe(null);
+    });
+  });
+
+  describe("query contacts", () => {
+    it("returns an empty array if nothing found in store", () => {
+      const query = new AddressBookQuery(
+        graph(),
+        sym("http://pod.test/alice/contacts/index.ttl#this"),
+      );
+      const result = query.queryContacts();
+      expect(result).toEqual([]);
+    });
+
+    it("returns a single contact with name and URI", () => {
+      const store = graph();
+      const addressBookNode = sym(
+        "http://pod.test/alice/contacts/index.ttl#this",
+      );
+      const peopleNode = sym("http://pod.test/alice/contacts/people.ttl");
+
+      store.add(
+        addressBookNode,
+        vcard("nameEmailIndex"),
+        peopleNode,
+        addressBookNode.doc(),
+      );
+      store.add(
+        sym("http://pod.test/alice/contacts/Person/1#this"),
+        vcard("inAddressBook"),
+        addressBookNode,
+        peopleNode,
+      );
+      store.add(
+        sym("http://pod.test/alice/contacts/Person/1#this"),
+        vcard("fn"),
+        "Alice",
+        peopleNode,
+      );
+      const query = new AddressBookQuery(
+        store,
+        sym("http://pod.test/alice/contacts/index.ttl#this"),
+      );
+      const result = query.queryContacts();
+      expect(result).toEqual([
+        {
+          uri: "http://pod.test/alice/contacts/Person/1#this",
+          name: "Alice",
+        },
+      ]);
+    });
+
+    it("returns all contacts with name and URI", () => {
+      const store = graph();
+      const addressBookNode = sym(
+        "http://pod.test/alice/contacts/index.ttl#this",
+      );
+      const peopleNode = sym("http://pod.test/alice/contacts/people.ttl");
+
+      store.add(
+        addressBookNode,
+        vcard("nameEmailIndex"),
+        peopleNode,
+        addressBookNode.doc(),
+      );
+      store.add(
+        sym("http://pod.test/alice/contacts/Person/1#this"),
+        vcard("inAddressBook"),
+        addressBookNode,
+        peopleNode,
+      );
+      store.add(
+        sym("http://pod.test/alice/contacts/Person/2#this"),
+        vcard("inAddressBook"),
+        addressBookNode,
+        peopleNode,
+      );
+      store.add(
+        sym("http://pod.test/alice/contacts/Person/1#this"),
+        vcard("fn"),
+        "Alice",
+        peopleNode,
+      );
+      store.add(
+        sym("http://pod.test/alice/contacts/Person/2#this"),
+        vcard("fn"),
+        "Bob",
+        peopleNode,
+      );
+      const query = new AddressBookQuery(
+        store,
+        sym("http://pod.test/alice/contacts/index.ttl#this"),
+      );
+      const result = query.queryContacts();
+      expect(result).toEqual([
+        {
+          uri: "http://pod.test/alice/contacts/Person/1#this",
+          name: "Alice",
+        },
+        {
+          uri: "http://pod.test/alice/contacts/Person/2#this",
+          name: "Bob",
+        },
+      ]);
+    });
+
+    it("does not return a contact from wrong name-email index", () => {
+      const store = graph();
+      const addressBookNode = sym(
+        "http://pod.test/alice/contacts/index.ttl#this",
+      );
+      const peopleNode = sym("http://pod.test/alice/contacts/people.ttl");
+      const wrongPeopleNode = sym("http://pod.test/alice/wrong/people.ttl");
+
+      store.add(
+        addressBookNode,
+        vcard("nameEmailIndex"),
+        peopleNode,
+        addressBookNode.doc(),
+      );
+      store.add(
+        sym("http://pod.test/alice/contacts/Person/1#this"),
+        vcard("inAddressBook"),
+        addressBookNode,
+        wrongPeopleNode,
+      );
+      store.add(
+        sym("http://pod.test/alice/contacts/Person/1#this"),
+        vcard("fn"),
+        "Alice",
+        peopleNode,
+      );
+      const query = new AddressBookQuery(
+        store,
+        sym("http://pod.test/alice/contacts/index.ttl#this"),
+      );
+      const result = query.queryContacts();
+      expect(result).toEqual([]);
+    });
+
+    it("does not return a name from wrong name-email index", () => {
+      const store = graph();
+      const addressBookNode = sym(
+        "http://pod.test/alice/contacts/index.ttl#this",
+      );
+      const peopleNode = sym("http://pod.test/alice/contacts/people.ttl");
+      const wrongPeopleNode = sym("http://pod.test/alice/wrong/people.ttl");
+
+      store.add(
+        addressBookNode,
+        vcard("nameEmailIndex"),
+        peopleNode,
+        addressBookNode.doc(),
+      );
+      store.add(
+        sym("http://pod.test/alice/contacts/Person/1#this"),
+        vcard("inAddressBook"),
+        addressBookNode,
+        peopleNode,
+      );
+      store.add(
+        sym("http://pod.test/alice/contacts/Person/1#this"),
+        vcard("fn"),
+        "Alice",
+        wrongPeopleNode,
+      );
+      const query = new AddressBookQuery(
+        store,
+        sym("http://pod.test/alice/contacts/index.ttl#this"),
+      );
+      const result = query.queryContacts();
+      expect(result).toEqual([
+        {
+          name: "",
+          uri: "http://pod.test/alice/contacts/Person/1#this",
+        },
+      ]);
+    });
+
+    it("does not return a contact from wrong address book", () => {
+      const store = graph();
+      const addressBookNode = sym(
+        "http://pod.test/alice/contacts/index.ttl#this",
+      );
+      const peopleNode = sym("http://pod.test/alice/contacts/people.ttl");
+      const wrongAddressBookNode = sym(
+        "http://pod.test/wrong/contacts/index.ttl#this",
+      );
+
+      store.add(
+        addressBookNode,
+        vcard("nameEmailIndex"),
+        peopleNode,
+        addressBookNode.doc(),
+      );
+      store.add(
+        sym("http://pod.test/alice/contacts/Person/1#this"),
+        vcard("inAddressBook"),
+        wrongAddressBookNode,
+        peopleNode,
+      );
+      store.add(
+        sym("http://pod.test/alice/contacts/Person/1#this"),
+        vcard("fn"),
+        "Alice",
+        peopleNode,
+      );
+      const query = new AddressBookQuery(
+        store,
+        sym("http://pod.test/alice/contacts/index.ttl#this"),
+      );
+      const result = query.queryContacts();
+      expect(result).toEqual([]);
     });
   });
 });
