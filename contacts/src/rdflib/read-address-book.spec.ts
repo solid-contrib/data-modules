8d30d19b650c2d8141cf4c96c1773a95e65a9171
Angelo Veltens <angelo.veltens@online.de>
2023-11-15 19:11:33 +0100

    fetch group index and return groups when reading address book

--- contacts/src/rdflib/read-address-book.spec.ts
+++ contacts/src/rdflib/read-address-book.spec.ts
@@ -112,6 +112,80 @@ describe("read existing address book", () => {
       },
     ]);
   });
+
+  it("all groups are listed", async () => {
+    const uri = "https://pod.test/alice/contacts/index.ttl#this";
+
+    const authenticatedFetch = jest.fn();
+
+    const contacts = new ContactsModuleRdfLib({
+      fetch: authenticatedFetch,
+    });
+
+    mockTurtleResponse(
+      authenticatedFetch,
+      "https://pod.test/alice/contacts/index.ttl",
+      `
+    @prefix vcard: <http://www.w3.org/2006/vcard/ns#>.
+    @prefix ab: <http://www.w3.org/ns/pim/ab#>.
+    @prefix dc: <http://purl.org/dc/elements/1.1/>.
+    @prefix xsd: <http://www.w3.org/2001/XMLSchema#>.
+  
+    <#this> a vcard:AddressBook;
+        dc:title "Alice's contacts";
+        vcard:nameEmailIndex <people.ttl>;
+        vcard:groupIndex <groups.ttl>.
+`,
+    );
+    mockTurtleResponse(
+      authenticatedFetch,
+      "https://pod.test/alice/contacts/people.ttl",
+      "",
+    );
+
+    mockTurtleResponse(
+      authenticatedFetch,
+      "https://pod.test/alice/contacts/groups.ttl",
+      `
+    @prefix vcard: <http://www.w3.org/2006/vcard/ns#>.
+
+    <https://pod.test/alice/contacts/index.ttl#this>
+        vcard:includesGroup
+            <https://pod.test/alice/contacts/Group/1/index.ttl#this>,
+            <https://pod.test/alice/contacts/Group/2/index.ttl#this>,
+            <https://pod.test/alice/contacts/Group/3/index.ttl#this>
+    .
+    
+    <https://pod.test/alice/contacts/Group/1/index.ttl#this>
+        a vcard:Group ;
+        vcard:fn            "Family" .
+        
+    <https://pod.test/alice/contacts/Group/2/index.ttl#this>
+        a vcard:Group ;
+        vcard:fn            "Friends" .
+    
+    <https://pod.test/alice/contacts/Group/3/index.ttl#this>
+        a vcard:Group ;
+        vcard:fn            "Work" .
+      `,
+    );
+
+    const result = await contacts.readAddressBook(uri);
+    expect(result.groups).toEqual([
+      {
+        uri: "https://pod.test/alice/contacts/Group/1/index.ttl#this",
+        name: "Family",
+      },
+      {
+        uri: "https://pod.test/alice/contacts/Group/2/index.ttl#this",
+        name: "Friends",
+      },
+      {
+        uri: "https://pod.test/alice/contacts/Group/3/index.ttl#this",
+        name: "Work",
+      },
+    ]);
+  });
 });
 
 function mockTurtleResponse(fetch: jest.Mock, uri: string, ttl: string) {
