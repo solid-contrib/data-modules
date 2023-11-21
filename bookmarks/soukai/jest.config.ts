b15371b9f6cda0fcd1b10c3da954c67d725ff6dd
Reza Soltani <soltanireza65@gmail.com>
2023-11-21 15:03:30 +0330

    force ts-node to transform js files and custom transformIgnorePatterns to transform files in node_modules

--- bookmarks/soukai/jest.config.ts
+++ bookmarks/soukai/jest.config.ts
@@ -1,9 +1,15 @@
 import type { JestConfigWithTsJest } from "ts-jest";
 
 const jestConfig: JestConfigWithTsJest = {
-  preset: "ts-jest/presets/js-with-ts",
+  preset: "ts-jest",
   testRegex: "\\.test\\.ts$",
   testEnvironment: "node",
+  transform: {
+    "^.+\\.[tj]s$": "ts-jest"
+  },
+  transformIgnorePatterns: [
+    "<rootDir>/node_modules/(?!soukai-solid-utils)"
+  ],
   setupFilesAfterEnv: ["<rootDir>/test/setup.ts"],
 };
 
