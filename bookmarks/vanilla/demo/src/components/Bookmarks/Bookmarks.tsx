d7f1e9c0189a14172165c17b144c5eeed04f7461
Reza Soltani <soltanireza65@gmail.com>
2023-11-24 18:00:30 +0330

    demo point to src

--- bookmarks/vanilla/demo/src/components/Bookmarks/Bookmarks.tsx
+++ bookmarks/vanilla/demo/src/components/Bookmarks/Bookmarks.tsx
@@ -23,7 +23,7 @@ import { FC, useEffect, useState } from "react";
 import {
   Bookmark,
   IBookmark,
-} from "../../../../dist";
+} from "../../../../src";
 
 type IProps = {};
 
@@ -51,11 +51,13 @@ const Bookmarks: FC<IProps> = ({ }) => {
 
   const handleSubmit = async () => {
     const updatedDataset = await Bookmark.create(
-      bookmarkTitle,
-      bookmarkLink,
+      {
+        title: bookmarkTitle,
+        link: bookmarkLink,
+      },
       session
     );
-    setBookmarks(updatedDataset);
+    // setBookmarks(updatedDataset);
     setbookmarkTitle("");
     setbookmarkLink("");
   };
@@ -127,7 +129,10 @@ const Bookmarks: FC<IProps> = ({ }) => {
                         DEL
                       </IconButton>
                       <IconButton
-                        onClick={async () => console.log(await Bookmark.update("sdvsdv", "sdvs", row.url, session))}
+                        onClick={async () => console.log(row.url , await Bookmark.update(row.url, {
+                          title: "bookmarkTitle",
+                          link: "bookmarkLink",
+                        }, session))}
                       >
                         UPD
                       </IconButton>
@@ -139,7 +144,7 @@ const Bookmarks: FC<IProps> = ({ }) => {
           </TableContainer>
         </CardContent>
       </Card>
-    </Box>
+    </Box >
   );
 };
 
