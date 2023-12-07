6426eb64e7e9c941108a1c0850aec65cdbb2507d
Reza Soltani <soltanireza65@gmail.com>
2023-12-07 14:57:55 +0330

    fix timestamps

--- bookmarks/vanilla/src/modules/Bookmark.ts
+++ bookmarks/vanilla/src/modules/Bookmark.ts
@@ -26,7 +26,7 @@ import {
     RDFS
 } from "@inrupt/vocab-common-rdf";
 import { namedNode } from '@rdfjs/data-model';
-import { __DC_UPDATED } from "../constants";
+import { __DC_UPDATED, __crdt_createdAt, __crdt_updatedAt, __crdt_resource } from "../constants";
 import { TypeIndexHelper } from "solid-typeindex-support";
 import { isValidUrl } from "../utils";
 
@@ -109,8 +109,8 @@ export class Bookmark {
 
                     return {
                         ...bookmark,
-                        ...(metadata?.createdAt && { created: metadata.createdAt }),
-                        ...(metadata?.createdAt && { updated: metadata.createdAt }),
+                        ...(metadata?.created && { created: metadata.created }),
+                        ...(metadata?.updated && { updated: metadata.updated }),
                     }
                 })
 
@@ -138,9 +138,16 @@ export class Bookmark {
 
         const thing = getThing(ds, url)
 
-        return thing ? this.mapBookmark(thing) : undefined
-    }
+        const meta = getThing(ds, `${url}-metadata`)
 
+        const metadata = meta ? this.mapBookmark(meta) : undefined as any
+
+        return thing ? {
+            ...this.mapBookmark(thing),
+            ...(metadata?.created && { created: metadata.created }),
+            ...(metadata?.updated && { updated: metadata.updated }),
+        } : undefined
+    }
 
     /**
      * Deletes a resource from the specified URL using the provided session.
@@ -259,7 +266,7 @@ export class Bookmark {
         const created = this.mapCreated(thing)
         const updated = this.mapUpdated(thing)
         const creator = this.mapCreator(thing)
-
+        const resource = getNamedNode(thing, __crdt_resource)?.value
         return {
             url,
             title,
@@ -268,6 +275,7 @@ export class Bookmark {
             ...(created && { created }),
             ...(updated && { updated }),
             ...(creator && { creator }),
+            ...(resource && { resource }),
         }
     }
     /**
@@ -303,7 +311,7 @@ export class Bookmark {
      * @return {string | undefined} The created date value, or undefined if not found.
      */
     private static mapCreated(thing: ThingPersisted): string | undefined {
-        return getLiteral(thing, DCTERMS.created)?.value
+        return getLiteral(thing, DCTERMS.created)?.value ?? getLiteral(thing, __crdt_createdAt)?.value
     }
     /**
      * Maps the updated value of a ThingPersisted object.
@@ -313,7 +321,7 @@ export class Bookmark {
      */
     private static mapUpdated(thing: ThingPersisted): string | undefined {
         return (
-            getLiteral(thing, __DC_UPDATED)?.value
+            getLiteral(thing, __DC_UPDATED)?.value ?? getLiteral(thing, __crdt_updatedAt)?.value
         );
     }
     /**
