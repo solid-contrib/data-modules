9f78f3ebbf76ff82fc20ad75f1dcd92e8d6b0f56
Angelo Veltens <angelo.veltens@online.de>
2023-12-17 14:03:59 +0100

    contacts: start independent solid server for e2e tests

--- contacts/jest.e2e.config.js
+++ contacts/jest.e2e.config.js
@@ -3,4 +3,5 @@ module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
   rootDir: "src/e2e-tests",
+  testTimeout: 60000,
 };
--- contacts/src/e2e-tests/contacts.e2e.spec.ts
+++ contacts/src/e2e-tests/contacts.e2e.spec.ts
@@ -1,20 +1,54 @@
 import { Fetcher, graph, UpdateManager } from "rdflib";
 import { ContactsModule } from "../rdflib";
 import { faker } from "@faker-js/faker";
+import { startServer } from "./start-server";
+import { v4 as uuid } from "uuid";
 
 describe("contacts module", () => {
-  beforeAll(async () => {
-    const response = await fetch("http://localhost:3000/alice/");
+  const testId = uuid();
+  beforeAll(async (): Promise<void> => {
+    await startServer(testId);
+  });
+
+  it("can fetch from the test server", async () => {
+    const response = await fetch("http://localhost:3456/");
     expect(response.status).toBe(200);
   });
+
+  it("can create a new address book", async () => {
+    const contacts = setupModule();
+    const uri = await contacts.createAddressBook({
+      container: "http://localhost:3456/",
+      name: "Personal contacts",
+    });
+    expect(uri).toMatch(
+      new RegExp("http://localhost:3456/[a-z\\-0-9]+/index.ttl#this"),
+    );
+  });
+
+  it("can read an existing address book", async () => {
+    const contacts = setupModule();
+    const result = await contacts.readAddressBook(
+      "http://localhost:3456/4243dbb6-3126-4bf9-9ea7-45e35c3c8d9d/index.ttl#this",
+    );
+    expect(result).toEqual({
+      uri: "http://localhost:3456/4243dbb6-3126-4bf9-9ea7-45e35c3c8d9d/index.ttl#this",
+      title: "Personal contacts",
+      contacts: [
+        {
+          uri: "http://localhost:3456/4243dbb6-3126-4bf9-9ea7-45e35c3c8d9d/Person/1973dcec-e71c-476c-87db-0d3332291214/index.ttl#this",
+          name: "Molly Braaten",
+        },
+      ],
+      groups: [],
+    });
+  });
+
   it("can create a new contact within an existing address book", async () => {
-    const store = graph();
-    const fetcher = new Fetcher(store);
-    const updater = new UpdateManager(store);
-    const contacts = new ContactsModule({ store, fetcher, updater });
+    const contacts = setupModule();
 
     const addressBook =
-      "http://localhost:3000/alice/public-write/ab9694d6-120e-415d-a315-90cd84c2e062/index.ttl#this";
+      "http://localhost:3456/4243dbb6-3126-4bf9-9ea7-45e35c3c8d9d/index.ttl#this";
 
     const name = faker.person.fullName();
     const uri = await contacts.createNewContact({
@@ -33,39 +67,43 @@ describe("contacts module", () => {
   });
 
   it("can read a existing contact", async () => {
-    const store = graph();
-    const fetcher = new Fetcher(store);
-    const updater = new UpdateManager(store);
-    const contacts = new ContactsModule({ store, fetcher, updater });
+    const contacts = setupModule();
 
     const contactUri =
-      "http://localhost:3000/alice/public-contacts/Person/1973dcec-e71c-476c-87db-0d3332291214/index.ttl#this";
+      "http://localhost:3456/4243dbb6-3126-4bf9-9ea7-45e35c3c8d9d/Person/1973dcec-e71c-476c-87db-0d3332291214/index.ttl#this";
 
     const result = await contacts.readContact(contactUri);
 
     expect(result).toEqual({
-      uri: "http://localhost:3000/alice/public-contacts/Person/1973dcec-e71c-476c-87db-0d3332291214/index.ttl#this",
+      uri: "http://localhost:3456/4243dbb6-3126-4bf9-9ea7-45e35c3c8d9d/Person/1973dcec-e71c-476c-87db-0d3332291214/index.ttl#this",
       name: "Molly Braaten",
       emails: [
         {
-          uri: "http://localhost:3000/alice/public-contacts/Person/1973dcec-e71c-476c-87db-0d3332291214/index.ttl#id1702497197769",
+          uri: "http://localhost:3456/4243dbb6-3126-4bf9-9ea7-45e35c3c8d9d/Person/1973dcec-e71c-476c-87db-0d3332291214/index.ttl#id1702497197769",
           value: "molly.braaten@gov.test",
         },
         {
-          uri: "http://localhost:3000/alice/public-contacts/Person/1973dcec-e71c-476c-87db-0d3332291214/index.ttl#id1702500031124",
+          uri: "http://localhost:3456/4243dbb6-3126-4bf9-9ea7-45e35c3c8d9d/Person/1973dcec-e71c-476c-87db-0d3332291214/index.ttl#id1702500031124",
           value: "molly.braaten@home.test",
         },
       ],
       phoneNumbers: [
         {
-          uri: "http://localhost:3000/alice/public-contacts/Person/1973dcec-e71c-476c-87db-0d3332291214/index.ttl#id1702497210116",
+          uri: "http://localhost:3456/4243dbb6-3126-4bf9-9ea7-45e35c3c8d9d/Person/1973dcec-e71c-476c-87db-0d3332291214/index.ttl#id1702497210116",
           value: "+1234567890",
         },
         {
-          uri: "http://localhost:3000/alice/public-contacts/Person/1973dcec-e71c-476c-87db-0d3332291214/index.ttl#id1702500092868",
+          uri: "http://localhost:3456/4243dbb6-3126-4bf9-9ea7-45e35c3c8d9d/Person/1973dcec-e71c-476c-87db-0d3332291214/index.ttl#id1702500092868",
           value: "+0987654321",
         },
       ],
     });
   });
 });
+
+function setupModule() {
+  const store = graph();
+  const fetcher = new Fetcher(store);
+  const updater = new UpdateManager(store);
+  return new ContactsModule({ store, fetcher, updater });
+}
