bac3a3062be5c36f0df3dd9b97d18aaf1ac0ae4a
Reza Soltani <soltanireza65@gmail.com>
2023-12-30 21:44:36 +0330

    change jest config to handle xternal deps

--- bookmarks/soukai/jest.config.ts
+++ bookmarks/soukai/jest.config.ts
@@ -1,14 +1,18 @@
 import type { JestConfigWithTsJest } from "ts-jest";
 
+const esModules = ['soukai-solid-utils', '@rezasoltani/solid-typeindex-support', '@noeldemartin/solid-utils'].join('|');
+
+
 const jestConfig: JestConfigWithTsJest = {
   preset: "ts-jest",
   testRegex: "\\.test\\.ts$",
   testEnvironment: "node",
   transform: {
-    "^.+\\.[tj]s$": "ts-jest"
+    "^.+\\.[tj]s$": "ts-jest",
+    [`(${esModules}).+\\.js$`]: 'ts-jest',
   },
   transformIgnorePatterns: [
-    "<rootDir>/node_modules/(?!soukai-solid-utils)"
+    `/node_modules/(?!${esModules})`
   ],
   setupFilesAfterEnv: ["<rootDir>/test/setup.ts"],
 };
