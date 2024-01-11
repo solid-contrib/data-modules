6769f01bdd1933b3f579933e0b7e610613ee92ad
Reza Soltani <soltanireza65@gmail.com>
2024-01-11 16:25:28 +0330

    ...

--- bookmarks/rdflib/demo/src/App.tsx
+++ bookmarks/rdflib/demo/src/App.tsx
@@ -1,195 +1,77 @@
-import { useEffect, useMemo } from "react";
-import { useUserSession } from "./atoms/userSession.atom";
-import { doLogin, doLogout, handleRedirectAfterLogin } from "./utils";
-import { Fetcher, UpdateManager, graph, namedNode, parse } from 'rdflib'
 import {
-  Box,
-  Button,
-  ButtonGroup,
-  Container,
-  Flex,
-  Input,
-  Link,
-  Table,
-  Tbody,
-  Td,
-  Text,
-  Th,
-  Thead,
-  Tr,
-} from "@chakra-ui/react";
-import * as $rdf from "rdflib";
+  Session,
+  getDefaultSession,
+  handleIncomingRedirect,
+  login,
+  logout,
+} from "@inrupt/solid-client-authn-browser";
+import { useEffect, useState } from "react";
+import { Fetcher, graph } from 'rdflib'
+const bookmarksURL = "https://solid-dm.solidcommunity.net/bookmarks/bookmark-formats.ttl"
 
-import { useState } from "react";
+async function startLogin() {
+  // Start the Login Process if not already logged in.
+  if (!getDefaultSession().info.isLoggedIn) {
+    await login({
+      oidcIssuer: "https://solidcommunity.net",
+      redirectUrl: new URL("/", window.location.href).toString(),
+      clientName: "My application",
+    });
+  }
+}
 
-// @ts-expect-error
-import solidNamespace from "solid-namespace";
-
-
-const ns = solidNamespace($rdf);
-
-function App() {
-  const { userSession, setUserSession } = useUserSession();
-  // const {  } = userSession
-  const [form, setForm] = useState({ title: "", link: "", topic: "" });
+export default function App() {
+  const [session, setSession] = useState<Session | undefined>(undefined);
+  const [isLoggedIn, setIsLoggedIn] = useState(session?.info.isLoggedIn ?? false);
 
   useEffect(() => {
     (async () => {
-      await handleRedirectAfterLogin(async (loggedInSession) => setUserSession(loggedInSession));
+      await handleIncomingRedirect({ restorePreviousSession: true });
+      const session = getDefaultSession();
+      setSession(session);
+      setIsLoggedIn(session.info.isLoggedIn);
     })();
   }, []);
 
-
-  const [store, fetcher, updater] = useMemo(() => {
-    const store = graph();
-    return [store, new Fetcher(store, { fetch: userSession?.fetch }), new UpdateManager(store)];
-  }, [userSession?.fetch]);
-
-  // userSession?.fetch
-  const [name, setName] = useState<string | undefined>();
-  const [error, setError] = useState<Error | null>(null);
-
-
-  const profileNode = namedNode(userSession?.info.webId!);
-
-  useEffect(() => {
-    fetcher.load(profileNode.doc())
-      .then(() => setName(store.any(profileNode, ns.foaf("name"), null)?.value ?? ""))
-      .catch(setError);
-  }, [store, profileNode, fetcher]);
   return (
-    <Container>
-
-      {!userSession?.info.isLoggedIn && (
-        <Box>
-          <Button onClick={doLogin}>Login</Button>
-        </Box>
-      )}
-
-      {userSession?.info.isLoggedIn && (
-        <Box>
-          <Button onClick={doLogout}>Logout</Button>
-          <Text>Logged in as:</Text>
-          <Link href={userSession?.info.webId}>
-            {userSession?.info.webId && userSession?.info.webId}
-          </Link>
-          <Flex gap={2}>
-            <Input
-              value={form?.title}
-              placeholder="title"
-              onChange={(e) =>
-                setForm((prev: any) => ({ ...prev, title: e.target.value }))
-              }
-            />
-            <Input
-              value={form?.topic}
-              placeholder="topic"
-              onChange={(e) =>
-                setForm((prev: any) => ({ ...prev, topic: e.target.value }))
-              }
-            />
-            <Input
-              value={form?.link}
-              placeholder="link"
-              onChange={(e) =>
-                setForm((prev: any) => ({ ...prev, link: e.target.value }))
-              }
-            />
-
-            <Button
-              onClick={async () => {
-                // const factory = await BookmarkFactory.getInstance(
-                //   {
-                //     webId: userSession?.info.webId ?? "",
-                //     fetch: userSession?.fetch,
-                //     isPrivate: true,
-                //   }
-                // );
-                // const bookmark = await factory.create(form);
-                // setForm({ title: "", link: "", topic: "" });
-                // const bookmarks = await factory.getAll();
-                // setBookmarks(bookmarks);
-              }}
-            >
-              ADD
-            </Button>
-          </Flex>
-          <div>
-            <Table
-              variant="striped"
-              size="sm"
-            >
-              <Thead>
-                <Tr>
-                  <Th>title</Th>
-                  <Th>topic</Th>
-                  <Th>Link</Th>
-                  <Th>actions</Th>
-                </Tr>
-              </Thead>
-              <Tbody>
-                {/* bookmarks */}
-                {([] as any).map((b: any, i: number) => (
-                  <Tr key={i}>
-                    <Td>{b.title}</Td>
-                    <Td>{b.topic}</Td>
-                    <Td>
-                      <a>{b.link}</a>
-                    </Td>
-                    <Td>
-                      <ButtonGroup variant="outline">
-                        <Button
-                          onClick={async () => {
-                            // const factory = await BookmarkFactory.getInstance({
-                            //   webId: userSession?.info.webId ?? "",
-                            //   fetch: userSession?.fetch,
-                            //   isPrivate: true,
-                            // });
-                            // const bookmark = await factory.get(b.url);
-                          }}
-                        >
-                          GET
-                        </Button>
-
-                        <Button
-                          onClick={async () => {
-                            // const factory = await BookmarkFactory.getInstance({
-                            //   webId: userSession?.info.webId ?? "",
-                            //   fetch: userSession?.fetch,
-                            //   isPrivate: true,
-                            // });
-                            // const bookmark = await factory.update(b.url, {
-                            //   ...(b as any),
-                            //   label: (Math.random() + 1).toString(36).substring(7),
-                            // });
-                          }}
-                        >
-                          UPD
-                        </Button>
-
-                        <Button
-                          onClick={async () => {
-                            // const factory = await BookmarkFactory.getInstance({
-                            //   webId: userSession?.info.webId ?? "",
-                            //   fetch: userSession?.fetch,
-                            //   isPrivate: true,
-                            // });
-                            // await factory.remove(b.url);
-                          }}
-                        >
-                          DEL
-                        </Button>
-                      </ButtonGroup>
-                    </Td>
-                  </Tr>
-                ))}
-              </Tbody>
-            </Table>
-          </div>
-        </Box>
-      )}
-    </Container>
+    <>
+      {session?.info.webId}
+      {isLoggedIn ? <AuthenticatedView session={session} /> : <GuestView />}
+      <a href="/">Home</a>
+    </>
   );
 }
 
-export default App;
+const GuestView = () => {
+  return (
+    <button onClick={startLogin}>Loggin</button>
+  )
+}
+
+
+const AuthenticatedView = ({ session }: { session?: Session }) => {
+
+
+  const handleGetBookmarks = async () => {
+    var store = graph()
+    var fetcher = new Fetcher(store, { timeout: 5000, fetch: session?.fetch })
+
+    fetcher.nowOrWhenFetched(bookmarksURL, function (ok: boolean, message: string, response?: any) {
+      if (!ok) {
+        console.log("Oops, something happened and couldn't fetch data " + message);
+      } else if (response.onErrorWasCalled || response.status !== 200) {
+        console.log('    Non-HTTP error reloading data! onErrorWasCalled=' + response.onErrorWasCalled + ' status: ' + response.status)
+      } else {
+        console.log("---data loaded---")
+        console.log("🚀 ~ response:", response.responseText)
+      }
+    })
+    // await session?.fetch(bookmarksURL);
+  }
+  return (
+    <div>
+      <button onClick={() => logout()}>Logout</button>
+      <button onClick={handleGetBookmarks}>GET</button>
+    </div>
+  )
+}
--- bookmarks/rdflib/demo/src/main.tsx
+++ bookmarks/rdflib/demo/src/main.tsx
@@ -1,15 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import { RecoilRoot } from "recoil";
-import { ChakraProvider } from "@chakra-ui/react";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RecoilRoot>
-      <ChakraProvider>
-        <App />
-      </ChakraProvider>
-    </RecoilRoot>
-  </React.StrictMode>
-);
+    <App />
+  </React.StrictMode>,
+)
