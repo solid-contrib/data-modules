71ce897130a7b484062b2233566fa428d1b43aad
Angelo Veltens <angelo.veltens@online.de>
2024-01-12 19:54:40 +0100

    contacts: readGroup example & changelog entry

--- contacts/CHANGELOG.md
+++ contacts/CHANGELOG.md
@@ -9,6 +9,7 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 ### Added
 
 - [createNewGroup](https://solid-contrib.github.io/data-modules/contacts-rdflib-api/interfaces/ContactsModule.html#createNewGroup)
+- [readGroup](https://solid-contrib.github.io/data-modules/contacts-rdflib-api/interfaces/ContactsModule.html#readGroup)
 
 ## 0.2.1
 
---
+++ contacts/examples/read-group.mjs
@@ -0,0 +1,10 @@
+import {ContactsModuleRdfLib as ContactsModule} from '../dist/index.js';
+import {Fetcher, graph, UpdateManager} from "rdflib";
+
+const store = graph()
+const fetcher = new Fetcher(store)
+const updater = new UpdateManager(store)
+const contacts = new ContactsModule({store, fetcher, updater})
+
+const result = await contacts.readGroup("http://localhost:3000/alice/public-contacts/Group/officials.ttl#this")
+console.log(result)
