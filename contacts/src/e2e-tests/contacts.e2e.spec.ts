4a72283749e8441fbf073e4a04ed259d7326a623
Angelo Veltens <angelo.veltens@online.de>
2024-02-07 18:38:54 +0100

    contacts: add failing e2e test for create contact within groups

--- contacts/src/e2e-tests/contacts.e2e.spec.ts
+++ contacts/src/e2e-tests/contacts.e2e.spec.ts
@@ -200,6 +200,47 @@ describe("contacts module", () => {
       ).toBe(false);
     }
   });
+
+  it("can create a new contact within existing groups", async () => {
+    // given an address book and two groups
+    const contacts = setupModule();
+
+    const addressBookUri =
+      "http://localhost:3456/4243dbb6-3126-4bf9-9ea7-45e35c3c8d9d/index.ttl#this";
+    const firstGroupUri =
+      "http://localhost:3456/4243dbb6-3126-4bf9-9ea7-45e35c3c8d9d/Group/88f4eb67-f510-49c8-8d52-9080cd3e489f/index.ttl#this";
+    const secondGroupUri =
+      "http://localhost:3456/4243dbb6-3126-4bf9-9ea7-45e35c3c8d9d/Group/1f0d98b1-5eac-4c44-b6e2-29d9784c40cb/index.ttl#this";
+
+    // when a new contact is created within the two groups
+    const name = faker.person.fullName();
+    const uri = await contacts.createNewContact({
+      addressBookUri,
+      contact: {
+        name: name,
+      },
+      groupUris: [firstGroupUri, secondGroupUri],
+    });
+
+    // then the address book contains the new contact
+    const result = await contacts.readAddressBook(addressBookUri);
+    expect(result.contacts).toContainEqual({
+      uri,
+      name,
+    });
+
+    // and both groups contain the new contact as well
+    const firstGroupResult = await contacts.readGroup(firstGroupUri);
+    expect(firstGroupResult.members).toContainEqual({
+      uri,
+      name,
+    });
+    const secondGroupResult = await contacts.readGroup(secondGroupUri);
+    expect(secondGroupResult.members).toContainEqual({
+      uri,
+      name,
+    });
+  });
 });
 
 function setupModule() {
