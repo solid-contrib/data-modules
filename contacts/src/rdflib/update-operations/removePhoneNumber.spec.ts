a782d0e08d42181adc26da9802edad0ac3c1e531
Angelo Veltens <angelo.veltens@online.de>
2024-03-15 23:07:12 +0100

    contacts: implement remove phone number update operation

---
+++ contacts/src/rdflib/update-operations/removePhoneNumber.spec.ts
@@ -0,0 +1,68 @@
+import { removePhoneNumber } from "./removePhoneNumber";
+import { graph, st, sym } from "rdflib";
+import { vcard } from "../namespaces";
+
+describe("removePhoneNumber", () => {
+  it("deletes any statement from the phone doc where the phone number is the subject", () => {
+    const store = graph();
+
+    const contactNode = sym("https://pod.test/contacts/1#this");
+    const phoneNode = sym("https://pod.test/contacts/1/phone#this");
+
+    store.add(
+      st(phoneNode, vcard("anything"), sym("tel:1234"), phoneNode.doc()),
+    );
+
+    const result = removePhoneNumber(contactNode, phoneNode, store);
+    expect(result.deletions).toContainEqual(
+      st(
+        sym("https://pod.test/contacts/1/phone#this"),
+        vcard("anything"),
+        sym("tel:1234"),
+        phoneNode.doc(),
+      ),
+    );
+  });
+
+  it("does not delete statements about the phone number in other known documeunts", () => {
+    const store = graph();
+
+    const contactNode = sym("https://pod.test/contacts/1#this");
+    const phoneNode = sym("https://pod.test/contacts/1/phone#this");
+
+    store.add(
+      st(
+        phoneNode,
+        vcard("anything"),
+        sym("tel:1234"),
+        sym("https://elsewhere.test"),
+      ),
+    );
+
+    const result = removePhoneNumber(contactNode, phoneNode, store);
+    expect(result.deletions).not.toContainEqual(
+      st(
+        phoneNode,
+        vcard("anything"),
+        sym("tel:1234"),
+        sym("https://elsewhere.test"),
+      ),
+    );
+  });
+
+  it("removes the link from contact to phone", () => {
+    const store = graph();
+
+    const contactNode = sym("https://pod.test/contacts/1#this");
+    const phoneNode = sym("https://pod.test/contacts/1/phone#this");
+
+    store.add(
+      st(contactNode, vcard("hasTelephone"), phoneNode, contactNode.doc()),
+    );
+
+    const result = removePhoneNumber(contactNode, phoneNode, store);
+    expect(result.deletions).toContainEqual(
+      st(contactNode, vcard("hasTelephone"), phoneNode, contactNode.doc()),
+    );
+  });
+});
