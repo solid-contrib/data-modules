178c45ac9ede0469283aad4dfeb2a5d6e8672854
Angelo Veltens <angelo.veltens@online.de>
2024-03-21 15:46:01 +0100

    contacts: query public type index

---
+++ contacts/src/rdflib/queries/ProfileQuery.spec.ts
@@ -0,0 +1,45 @@
+import { graph, lit, sym } from "rdflib";
+import { ProfileQuery } from "./ProfileQuery";
+import { solid } from "../namespaces";
+
+describe("ProfileQuery", () => {
+  describe("query public type index", () => {
+    it("returns null if store is empty", () => {
+      const store = graph();
+      const profileNode = sym("https://pod.test/alice/profile/card#me");
+      const query = new ProfileQuery(profileNode, store);
+      const result = query.queryPublicTypeIndex();
+      expect(result).toBeNull();
+    });
+
+    it("returns node of public type index", () => {
+      const store = graph();
+      const profileNode = sym("https://pod.test/alice/profile/card#me");
+      store.add(
+        profileNode,
+        solid("publicTypeIndex"),
+        sym("https://pod.test/alice/settings/publicTypeIndex.ttl"),
+        profileNode.doc(),
+      );
+      const query = new ProfileQuery(profileNode, store);
+      const result = query.queryPublicTypeIndex();
+      expect(result).toEqual(
+        sym("https://pod.test/alice/settings/publicTypeIndex.ttl"),
+      );
+    });
+
+    it("returns null if value is not a named node", () => {
+      const store = graph();
+      const profileNode = sym("https://pod.test/alice/profile/card#me");
+      store.add(
+        profileNode,
+        solid("publicTypeIndex"),
+        lit("https://pod.test/alice/settings/publicTypeIndex.ttl"),
+        profileNode.doc(),
+      );
+      const query = new ProfileQuery(profileNode, store);
+      const result = query.queryPublicTypeIndex();
+      expect(result).toBeNull();
+    });
+  });
+});
---
+++ contacts/src/rdflib/queries/ProfileQuery.ts
@@ -0,0 +1,21 @@
+import { IndexedFormula, isNamedNode, NamedNode } from "rdflib";
+import { solid } from "../namespaces.js";
+
+export class ProfileQuery {
+  constructor(
+    private profileNode: NamedNode,
+    private store: IndexedFormula,
+  ) {}
+
+  queryPublicTypeIndex(): NamedNode | null {
+    const node = this.store.any(
+      this.profileNode,
+      solid("publicTypeIndex"),
+      null,
+    );
+    if (isNamedNode(node)) {
+      return node as NamedNode;
+    }
+    return null;
+  }
+}
