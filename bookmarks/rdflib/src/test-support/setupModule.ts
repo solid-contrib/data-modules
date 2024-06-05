b56d734ec8b03f17638787a9c98058f9d4cac038
Angelo Veltens <angelo.veltens@online.de>
2024-06-05 22:03:02 +0200

    bookmarks: fix imports

--- bookmarks/rdflib/src/test-support/setupModule.ts
+++ bookmarks/rdflib/src/test-support/setupModule.ts
@@ -1,5 +1,5 @@
 import { Fetcher, graph, UpdateManager } from "rdflib";
-import { BookmarksModuleRdfLib } from "../module/BookmarksModuleRdfLib";
+import { BookmarksModuleRdfLib } from "../module/BookmarksModuleRdfLib.js";
 
 export function setupModule(authenticatedFetch: typeof fetch = fetch) {
   const store = graph();
