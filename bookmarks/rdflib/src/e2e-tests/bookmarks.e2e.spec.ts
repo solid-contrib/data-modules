97dbee310b9f343988ba4c051b079627709fcf75
Angelo Veltens <angelo.veltens@online.de>
2024-06-06 19:50:04 +0200

    bookmarks: add failing e2e test to create bookmark within a document

--- bookmarks/rdflib/src/e2e-tests/bookmarks.e2e.spec.ts
+++ bookmarks/rdflib/src/e2e-tests/bookmarks.e2e.spec.ts
@@ -4,12 +4,24 @@ describe("bookmarks", () => {
   it("can create a new bookmark in a container", async () => {
     const bookmarks = setupModule();
     const uri = await bookmarks.createBookmark({
-      containerUri: "http://localhost:3456/bookmarks/",
+      storageUrl: "http://localhost:3456/bookmarks/",
       title: "My favorite website",
       url: "https://favorite.example",
     });
     expect(uri).toMatch(
       new RegExp("http://localhost:3456/bookmarks/[a-zA-Z0-9]{6}#it"),
+    );
+  });
+
+  it("can create a new bookmark in an existing document", async () => {
+    const bookmarks = setupModule();
+    const uri = await bookmarks.createBookmark({
+      storageUrl: "http://localhost:3456/public/bookmarks",
+      title: "My favorite website",
+      url: "https://favorite.example",
+    });
+    expect(uri).toMatch(
+      new RegExp("http://localhost:3456/public/bookmarks#[a-zA-Z0-9]{6}"),
     );
   });
 });
