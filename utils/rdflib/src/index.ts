9ccd7db9024edd17db94e1e4fa8e7d51407fc3e4
Angelo Veltens <angelo.veltens@online.de>
2024-07-03 20:30:31 +0200

    rdflib-utils: export namespaces

--- utils/rdflib/src/index.ts
+++ utils/rdflib/src/index.ts
@@ -5,6 +5,7 @@ export * from "./identifier/index.js";
 export * from "./queries/index.js";
 export * from "./module/index.js";
 export * from "./update-operations/index.js";
+export * from "./namespaces/index.js";
 
 export interface ModuleConfig {
   store: IndexedFormula;
