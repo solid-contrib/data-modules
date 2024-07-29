ddf9dd6c5781ded48cdc13f484c19035fc7ed174
Angelo Veltens <angelo+github.com@veltens.org>
2024-07-29 18:34:29 +0200

    bookmarks-rdflib: test returning multiple bookmarks

--- bookmarks/rdflib/src/module/queries/BookmarkQuery.spec.ts
+++ bookmarks/rdflib/src/module/queries/BookmarkQuery.spec.ts
@@ -46,5 +46,61 @@ describe(BookmarkQuery.name, () => {
         },
       ]);
     });
+
+    it("returns multiple bookmarks", () => {
+      const store = graph();
+      store.add(
+        sym("https://pod.test/bookmarks.ttl#1"),
+        rdf("type"),
+        bookm("Bookmark"),
+        sym("https://pod.test/bookmarks.ttl"),
+      );
+      store.add(
+        sym("https://pod.test/bookmarks.ttl#1"),
+        dct("title"),
+        lit("Bookmark 1"),
+        sym("https://pod.test/bookmarks.ttl"),
+      );
+      store.add(
+        sym("https://pod.test/bookmarks.ttl#1"),
+        bookm("recalls"),
+        sym("https://bookmarked-url-1.test"),
+        sym("https://pod.test/bookmarks.ttl"),
+      );
+      store.add(
+        sym("https://pod.test/bookmarks.ttl#2"),
+        rdf("type"),
+        bookm("Bookmark"),
+        sym("https://pod.test/bookmarks.ttl"),
+      );
+      store.add(
+        sym("https://pod.test/bookmarks.ttl#2"),
+        dct("title"),
+        lit("Bookmark 2"),
+        sym("https://pod.test/bookmarks.ttl"),
+      );
+      store.add(
+        sym("https://pod.test/bookmarks.ttl#2"),
+        bookm("recalls"),
+        sym("https://bookmarked-url-2.test"),
+        sym("https://pod.test/bookmarks.ttl"),
+      );
+      const result = new BookmarkQuery(
+        sym("https://pod.test/bookmarks.ttl"),
+        store,
+      ).queryBookmarks();
+      expect(result).toEqual([
+        {
+          uri: "https://pod.test/bookmarks.ttl#1",
+          title: "Bookmark 1",
+          bookmarkedUrl: "https://bookmarked-url-1.test",
+        },
+        {
+          uri: "https://pod.test/bookmarks.ttl#2",
+          title: "Bookmark 2",
+          bookmarkedUrl: "https://bookmarked-url-2.test",
+        },
+      ]);
+    });
   });
 });
