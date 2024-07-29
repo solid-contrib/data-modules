5e32344589177718f467ed8f6063074ae9baec88
Angelo Veltens <angelo+github.com@veltens.org>
2024-07-29 21:14:28 +0200

    rdflib-utils: fix missing export

--- utils/rdflib/src/queries/index.ts
+++ utils/rdflib/src/queries/index.ts
@@ -1,3 +1,4 @@
 export { PreferencesQuery } from "./PreferencesQuery.js";
 export { ProfileQuery } from "./ProfileQuery.js";
 export { TypeIndexQuery } from "./TypeIndexQuery.js";
+export { ContainerQuery } from './ContainerQuery.js';
