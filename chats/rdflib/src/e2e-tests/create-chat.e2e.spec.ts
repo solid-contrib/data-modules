e2515258ff7d3a045e95ccd14c142afd3a6175d4
Angelo Veltens <angelo+github.com@veltens.org>
2024-08-12 18:48:37 +0200

    chats: mint and return URI for new chat

--- chats/rdflib/src/e2e-tests/create-chat.e2e.spec.ts
+++ chats/rdflib/src/e2e-tests/create-chat.e2e.spec.ts
@@ -1,8 +1,15 @@
 import { setupModule } from "../test-support/setupModule";
 
 describe("chats", () => {
-  it("module can be created", () => {
+  it("creates a new chat", () => {
     const chats = setupModule();
-    expect(chats).toBeDefined()
+    const uri = chats.createChat({
+      containerUri: "http://localhost:3456/alice/chats/",
+      name: "A new chat"
+    })
+    expect(uri).toMatch(
+      new RegExp("http://localhost:3456/alice/chats/[a-zA-Z0-9]{6}/index.ttl#this"),
+    );
   });
+
 });
--- chats/rdflib/src/index.ts
+++ chats/rdflib/src/index.ts
@@ -5,4 +5,24 @@ export default ChatsModuleRdfLib;
 
 export interface ChatsModule {
 
+  /**
+   * Creates a new chat in the given container
+   * @param command
+   * @return The URI of the newly created chat
+   */
+  createChat(command: CreateChatCommand): string
+}
+
+/**
+ * Data needed to create a new chat
+ */
+export interface CreateChatCommand {
+  /**
+   * The URI of the target container
+   */
+  containerUri: string,
+  /**
+   * The human-readable title for the chat
+   */
+  name: string,
 }
