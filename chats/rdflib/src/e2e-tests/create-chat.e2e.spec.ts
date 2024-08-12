9cea2244aeaef431290ee454a6f7e12c1a3ea835
Angelo Veltens <angelo+github.com@veltens.org>
2024-08-12 19:28:15 +0200

    chats: add missing await to e2e test

--- chats/rdflib/src/e2e-tests/create-chat.e2e.spec.ts
+++ chats/rdflib/src/e2e-tests/create-chat.e2e.spec.ts
@@ -1,15 +1,16 @@
 import { setupModule } from "../test-support/setupModule";
 
 describe("chats", () => {
-  it("creates a new chat", () => {
+  it("creates a new chat", async () => {
     const chats = setupModule();
-    const uri = chats.createChat({
+    const uri = await chats.createChat({
       containerUri: "http://localhost:3456/alice/chats/",
-      name: "A new chat"
-    })
+      name: "A new chat",
+    });
     expect(uri).toMatch(
-      new RegExp("http://localhost:3456/alice/chats/[a-zA-Z0-9]{6}/index.ttl#this"),
+      new RegExp(
+        "http://localhost:3456/alice/chats/[a-zA-Z0-9]{6}/index.ttl#this",
+      ),
     );
   });
-
 });
