218afcddb8df095d9ffff672d6e9313cabf5a2ec
Angelo Veltens <angelo+github.com@veltens.org>
2024-08-26 18:39:57 +0200

    chats: e2e test and fake implementation for latest messages

--- chats/rdflib/src/e2e-tests/read-chat.e2e.spec.ts
+++ chats/rdflib/src/e2e-tests/read-chat.e2e.spec.ts
@@ -1,7 +1,7 @@
 import { setupModule } from "../test-support/setupModule";
 
-describe("chats", () => {
-  it("read a chat", async () => {
+describe("read a chat", () => {
+  it("retrieves the chat name", async () => {
     const chats = setupModule();
 
     const chat = await chats.readChat(
@@ -12,5 +12,24 @@ describe("chats", () => {
       "http://localhost:3456/chats/eBIszJ/index.ttl#this",
     );
     expect(chat.name).toEqual("An existing chat channel");
+  });
+
+  it("retrieves the latest messages", async () => {
+    const chats = setupModule();
+
+    const chat = await chats.readChat(
+      "http://localhost:3456/chats/eBIszJ/index.ttl#this",
+    );
+
+    expect(chat.uri).toEqual(
+      "http://localhost:3456/chats/eBIszJ/index.ttl#this",
+    );
+    expect(chat.latestMessages).toEqual([
+      {
+        text: "Hello visitor, welcome to my public chat lobby!",
+        date: new Date("2024-07-01T17:47:14Z"),
+        authorWebId: "http://localhost:3000/alice/profile/card#me",
+      },
+    ]);
   });
 });
