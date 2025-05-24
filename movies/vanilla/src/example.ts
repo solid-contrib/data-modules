81e3ee4451e019180ad49b0d1cd0a3863861a930
Michiel de Jong <michiel@pondersource.com>
2025-05-24 10:57:34 +0200

    try-catch around fetch

--- movies/vanilla/src/example.ts
+++ movies/vanilla/src/example.ts
@@ -1,6 +1,6 @@
 import 'dotenv/config';
 import { v7 } from 'css-authn';
-import { fetchList } from './main.js';
+import { fetchList, Interpretation } from './main.js';
 
 let authenticatedFetch: Awaited<ReturnType<typeof v7.getAuthenticatedFetch>>;
 
@@ -13,7 +13,15 @@ try {
 } catch(e) {
     console.log('Could not connect to your pod');
 }
-const listUrl = 'https://michielbdejong.solidcommunity.net/movies/';
-const interpretation = await fetchList(listUrl, authenticatedFetch);
+
+let interpretation: Interpretation;
+try {
+
+    const listUrl = 'https://michielbdejong.solidcommunity.net/movies/';
+    interpretation = await fetchList(listUrl, authenticatedFetch);
+} catch(e) {
+    console.error('Could not fetch your movies folder');
+}
 interpretation.listings.forEach(listing => console.log(listing));
 interpretation.watchActions.forEach(watchAction => console.log(watchAction));
+
