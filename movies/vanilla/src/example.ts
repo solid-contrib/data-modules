c407d6db383f3456a2c85472e657e1602be05d54
Michiel de Jong <michiel@pondersource.com>
2025-05-24 10:54:43 +0200

    try-catch around connect

--- movies/vanilla/src/example.ts
+++ movies/vanilla/src/example.ts
@@ -2,11 +2,17 @@ import 'dotenv/config';
 import { v7 } from 'css-authn';
 import { fetchList } from './main.js';
 
-const authenticatedFetch = await v7.getAuthenticatedFetch({
-  email: process.env.SOLID_EMAIL,
-  password: process.env.SOLID_PASSWORD,
-  provider: process.env.SOLID_SERVER,
-});
+let authenticatedFetch: Awaited<ReturnType<typeof v7.getAuthenticatedFetch>>;
+
+try {
+    authenticatedFetch = await v7.getAuthenticatedFetch({
+        email: process.env.SOLID_EMAIL,
+        password: process.env.SOLID_PASSWORD,
+        provider: process.env.SOLID_SERVER,
+    });
+} catch(e) {
+    console.log('Could not connect to your pod');
+}
 const listUrl = 'https://michielbdejong.solidcommunity.net/movies/';
 const interpretation = await fetchList(listUrl, authenticatedFetch);
 interpretation.listings.forEach(listing => console.log(listing));
